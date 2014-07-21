8b3cd6147e0aecf649d0e1386c11e323c232005c
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-21 15:55:32 -0400

    Added another route definition to custom API.

--- server.js
+++ server.js
@@ -12,11 +12,18 @@ var app = express();
 var router = express.Router();
 
 // Router will handle any requests with this endpoint depending on where router is "use()'d.
-router.get('/', function(req, res) {
+router.get('/hello', function(req, res) {
     // Returns a JSON response when user visits this endpoint
     res.json({
         title: 'Hello API',
         message: 'How are you?'
+    });
+});
+
+router.get('/bye', function(req, res) {
+    res.json({
+        title: 'Goodbye API',
+        message: 'See you soon.'
     });
 });
 
