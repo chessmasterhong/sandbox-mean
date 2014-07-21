96760027b92b16a4b639d84184d4576f52282c3a
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-21 11:03:33 -0400

    Added simple Express routing definitions to custom API.

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ var PORT = 8080;
 
 var app = express();
 
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({
+        title: 'Hello API',
+        message: 'How are you?'
+    });
+});
+
+app.use('/api', router);
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
     console.log('Express server listening on port ' + PORT);
 });
+
